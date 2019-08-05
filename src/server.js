bf5fcc40dc31b3ae0af3cb9fd697441b13e0b43e
Roberto Rivas <rr10rivas@gmail.com>
2019-08-04 22:59:29 -0400

    Adding environment variables

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import express, { json } from 'express';
+import dotenv from "dotenv";
 import IndexRoutes from "./routes/index.routes";
 import TaskRoutes from "./routes/task.routes";
 
+
+// Environment variables
+dotenv.config()
 
 // Up server
 const app = express();
